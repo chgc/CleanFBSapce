4e35273761097e4fea14ce7ef3b738089d10cf0a
Kevin Yang <chgc_kevin@hotmail.com>
2021-08-21 19:07:32 +0800

    Update clean-FB-recommand.user.js

--- clean-FB-recommand.user.js
+++ clean-FB-recommand.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Clean FB Suggested for you
 // @namespace    https://github.com/chgc/CleanFBSapce
-// @version      0.2
+// @version      0.3
 // @description  移除 FB 為你推薦
 // @author       Kevin Yang
 // @include      /https:\/\/www.facebook.com
@@ -9,10 +9,17 @@
 // @downloadURL  https://github.com/chgc/CleanFBSapce/raw/master/clean-FB-recommand.user.js
 // ==/UserScript==
 
-const NeedToRemoveKeywords = ['為你推薦', 'Suggested for you'];
+const NeedToRemoveKeywords = ['為你推薦', 'Suggested for you', '贊助'];
 
 function checkKeywordsExist(node) {
-  return NeedToRemoveKeywords.some((lang) => node.innerHTML.contains('dir="auto">' + lang + '</span>'));
+  return NeedToRemoveKeywords.some((lang) => node.innerHTML.contains('dir="auto">' + lang + '</span>')) || NeedToRemoveKeywords.some((lang) => node.innerHTML.contains('">' + lang + '<')) ;
+}
+
+function checkKeywordExistBySpan(node){
+   const id = node.querySelector('div[role=article]').getAttribute('aria-describedby').split(' ')[0];
+   if(id == null) return false;
+   const span = node.querySelector(`span[id=${id}]`);
+   return NeedToRemoveKeywords.some((lang) => span.innerHTML.contains(lang));
 }
 
 function removeRecommandPost() {
@@ -20,17 +27,19 @@ function removeRecommandPost() {
     if (checkKeywordsExist(node)) {
       node.remove();
     }
+    if (checkKeywordExistBySpan(node)){
+      node.remove();
+    }
   });
 }
 
 function executeActions() {
   removeRecommandPost();
-  setTimeout(executeActions, 200);
+  setTimeout(()=> executeActions(), 200);
 }
 
 (function () {
   'use strict';
-
   // Your code here...
   executeActions();
 })();
