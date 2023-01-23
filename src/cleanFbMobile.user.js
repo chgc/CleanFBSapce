454c947d8d198d3df81c952ef0f8b3680ba0123e
houcheng <houcheng@gmail.com>
2023-01-24 04:14:05 +0800

    Refactor code

--- src/cleanFbMobile.user.js
+++ src/cleanFbMobile.user.js
@@ -22,58 +22,46 @@ function checkKeywordExistBySpanInMobile(node) {
     if (spans.filter(s => s.innerText && s.innerText.startsWith("贊助")).length > 0) return true;
     return false;
 }
+
 var count = 0;
 var removed = [];
+var lastRunTick = (new Date()).getTime();
 
-function removeNode(node) {
-    if (node.innerText) {
-        console.log("remove node: ", node.innerText.split("\n")[0]);
-        node.style.background = 'black';
-        node.style.visibility = 'hidden';
-    } else {
-        console.log("node no text "); // should not delete
-        node.style.background = 'black';
-        node.style.visibility = 'hidden';
-    }
+function hideNode(node) {
+    console.log("Hide node: ", node.innerText ? node.innerText.split("\n")[0] : "node without text");
+    node.style.background = 'black';
+    node.style.visibility = 'hidden';
     if (count > 0) count = count - 1;
 }
 
-var lastRunTick = (new Date()).getTime();
 function removeRecommandPost() {
-  // console.log("removeRecommandPost");
+    var nowTick = (new Date()).getTime();
+    if (nowTick - lastRunTick < CheckInterval) return;
+    lastRunTick = nowTick;
 
-  var nowTick = (new Date()).getTime();
-  if (nowTick - lastRunTick < CheckInterval) return;
-  lastRunTick = nowTick;
-  // console.log("removeRecommandPost enter");
+    const s3Nodes = Array.from(document.querySelectorAll(".bg-s3"));
+    for (var i = 0; i < s3Nodes.length; i ++) {
+        const node = s3Nodes[i];
+        if (node.style.visibility === 'hidden') {
+            // do nothing if already hidden
+        } else if (count > 0) {
+            hideNode(node);
+        } else if (checkKeywordExistBySpanInMobile(node)) {
+            console.log("Hide by span", node);
+            hideNode(node);
+            count = 4;
+        }
+    };
 
-  const s3Nodes = Array.from(document.querySelectorAll(".bg-s3"));
-
-  for (var i = 0; i < s3Nodes.length; i ++) {
-    const node = s3Nodes[i];
-    if (node.style.visibility === 'hidden') {
-      // do nothing if already hidden
-      // console.log("skip already hidden");
-    } else if (count > 0) {
-      removeNode(node);
-    } else if (checkKeywordExistBySpanInMobile(node)) {
-      console.log("remove by span", node);
-      removeNode(node);
-      count = 4;
+    const storyNodes = Array.from(document.querySelectorAll(".story_body_container"));
+    for (var j = 0; j < storyNodes.length; j ++) {
+        const node = storyNodes[j];
+        // console.log("story_body_container with text: ", node.innerText.split('\n')[0]);
+        if (checkKeywordExistBySpanInMobile(node)) {
+            console.log("Hide by span in story_body_container", node);
+            hideNode(node);
+        }
     }
-  };
-
-  // document.querySelectorAll(".story_body_container").forEach((node) => {
-  // Tablet
-  const storyNodes = Array.from(document.querySelectorAll(".story_body_container"));
-  for (var j = 0; j < storyNodes.length; j ++) {
-    const node = storyNodes[j];
-    // console.log("story_body_container with text: ", node.innerText.split('\n')[0]);
-    if(checkKeywordExistBySpanInMobile(node)) {
-        console.log("remove by span in story_body_container", node);
-        removeNode(node);
-    }
-  }
 }
 
 function executeActions() {
